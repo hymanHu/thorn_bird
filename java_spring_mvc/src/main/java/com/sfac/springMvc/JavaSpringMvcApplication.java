fb1fab8794c9cba10cc249e16bdcf081a48b8e97
hymanHu <hujiangyx@163.com>
2020-12-08 16:31:27 +0800

    "搭建 spring mvc 框架，引入内嵌 tomcat，引入 log4j"

---
+++ java_spring_mvc/src/main/java/com/sfac/springMvc/JavaSpringMvcApplication.java
@@ -0,0 +1,45 @@
+package com.sfac.springMvc;
+
+import java.io.File;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+
+/**
+ * Description: Java Spring Mvc Application
+ * @author HymanHu
+ * @date 2020-12-08 13:27:58
+ */
+public class JavaSpringMvcApplication {
+
+	private static int PORT = 80;
+    private static String CONTEXT_PATH = "/";
+
+	public static void main(String[] args) throws LifecycleException {
+//		String baseDir = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+		String baseDir = new File("target/classes").getAbsolutePath();
+		String appDir = new File("src/main/webapp").getAbsolutePath();
+		System.out.println(baseDir);
+		System.out.println(appDir);
+		
+		// 创建 Tomcat，并设置基础目录、端口、连接器
+		Tomcat tomcat = new Tomcat();
+		tomcat.setBaseDir(baseDir);
+		tomcat.setPort(PORT);
+		tomcat.getConnector();
+		
+		// 添加 webapp
+        Context context = tomcat.addWebapp(CONTEXT_PATH, appDir);
+        WebResourceRoot resources = new StandardRoot(context);
+        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes", baseDir, "/"));
+        context.setResources(resources);
+        
+        // 启动服务器
+        tomcat.start();
+        tomcat.getServer().await();
+	}
+}
---
+++ java_spring_mvc/src/main/java/com/sfac/springMvc/controller/TestController.java
@@ -0,0 +1,44 @@
+package com.sfac.springMvc.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Description: Test Controller
+ * @author HymanHu
+ * @date 2020-12-08 10:10:23
+ */
+@Controller
+@RequestMapping("/test")
+public class TestController {
+	
+	private final static Logger LOGGER = LogManager.getLogger(TestController.class);
+	
+	/**
+	 * 127.0.0.1:8080/java_spring_mvc/test/desc ---- get
+	 */
+	@GetMapping("/desc")
+	@ResponseBody
+	public String testDesc() {
+		return "This is test module desc.";
+	}
+	
+	/**
+	 * 127.0.0.1/test/logger ---- get
+	 */
+	@GetMapping("/logger")
+	@ResponseBody
+	public String loggerTest() {
+		LOGGER.trace("This is trace logger.");
+		LOGGER.debug("This is debug logger.");
+		LOGGER.info("This is info logger.");
+		LOGGER.warn("This is warn logger.");
+		LOGGER.error("This is error logger.");
+		return "This is logger test.";
+	}
+
+}
