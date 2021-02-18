69ed8d3f510b1ec4c3afe17118458962b5932318
hymanHu <hujiangyx@163.com>
2021-02-18 10:16:04 +0800

    "Log 配置、多环境配置、热部署配置"

---
+++ java_spring_boot/src/main/java/com/sfac/springBoot/config/ApplicationTest.java
@@ -0,0 +1,57 @@
+package com.sfac.springBoot.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+/**
+ * Description: Application Test
+ * Component ---- 注册为 Bean
+ * PropertySource ---- 指定配置文件位置
+ * ConfigurationProperties ---- 指定公有前缀
+ * -有警告信息，Pom 导入 spring-boot-configuration-processor
+ * @author HymanHu
+ * @date 2021-02-18 08:52:40
+ */
+@Component
+@PropertySource("classpath:config/applicationTest.properties")
+@ConfigurationProperties(prefix = "com.test")
+public class ApplicationTest {
+
+	private String name;
+	private int age;
+	private String description;
+	private String random;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getRandom() {
+		return random;
+	}
+
+	public void setRandom(String random) {
+		this.random = random;
+	}
+}
--- java_spring_boot/src/main/java/com/sfac/springBoot/modules/test/controller/TestController.java
+++ java_spring_boot/src/main/java/com/sfac/springBoot/modules/test/controller/TestController.java
@@ -1,10 +1,15 @@
 package com.sfac.springBoot.modules.test.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sfac.springBoot.config.ApplicationTest;
 
 /**
  * @Description: Test Controller
@@ -14,6 +19,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/test")
 public class TestController {
+	
+	private final static Logger LOGGER = LoggerFactory.getLogger(TestController.class);
 	
 	@Value("${server.port}")
 	private int port;
@@ -25,6 +32,23 @@ public class TestController {
 	private String description;
 	@Value("${com.sfac.random}")
 	private String random;
+	
+	@Autowired
+	private ApplicationTest applicationTest;
+	
+	/**
+	 * 127.0.0.1/test/logger ---- get
+	 */
+	@GetMapping("/logger")
+	@ResponseBody
+	public String loggerTest() {
+		LOGGER.trace("This is trace log.");
+		LOGGER.debug("This is debug log.");
+		LOGGER.info("This is info log.");
+		LOGGER.warn("This is warn log.");
+		LOGGER.error("This is error log.");
+		return "This is logger test.";
+	}
 	
 	/**
 	 * 127.0.0.1/test/config ---- get
@@ -38,6 +62,10 @@ public class TestController {
 			.append(age).append("----")
 			.append(description).append("----")
 			.append(random).append("<br>");
+		sb.append(applicationTest.getName()).append("----")
+			.append(applicationTest.getAge()).append("----")
+			.append(applicationTest.getDescription()).append("----")
+			.append(applicationTest.getRandom()).append("<br>");
 		return sb.toString();
 	}
 	
@@ -47,7 +75,7 @@ public class TestController {
 	@GetMapping("/helloWorld")
 	@ResponseBody
 	public String helloWorld() {
-		return "Hello World!";
+		return "Hello World!!!!!!!";
 	}
 
 }
