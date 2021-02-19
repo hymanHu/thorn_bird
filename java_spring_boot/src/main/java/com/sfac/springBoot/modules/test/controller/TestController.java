9aaa4f4a8c9ce992d8514c6279a3ac99cb76c2f2
hymanHu <hujiangyx@163.com>
2021-02-19 10:59:01 +0800

    "文件上传"

--- java_spring_boot/src/main/java/com/sfac/springBoot/modules/test/controller/TestController.java
+++ java_spring_boot/src/main/java/com/sfac/springBoot/modules/test/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.sfac.springBoot.modules.test.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -12,11 +14,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.sfac.springBoot.config.ApplicationTest;
+import com.sfac.springBoot.config.ResourceConfigBean;
 import com.sfac.springBoot.modules.test.entity.City;
 import com.sfac.springBoot.modules.test.entity.Country;
 import com.sfac.springBoot.modules.test.service.CityService;
@@ -50,6 +56,78 @@ public class TestController {
 	private CityService cityService;
 	@Autowired
 	private CountryService countryService;
+	@Autowired
+	private ResourceConfigBean resourceConfigBean;
+	
+	/**
+	 * 127.0.0.1/test/files ---- post
+	 */
+	@PostMapping(value="/files", consumes="multipart/form-data")
+	public String uploadFiles(@RequestParam MultipartFile[] files, 
+			RedirectAttributes redirectAttributes) {
+		boolean isEmpty = true;
+		
+		try {
+			for (MultipartFile file : files) {
+				if (file.isEmpty()) {
+					continue;
+				}
+				
+				String fileName = file.getOriginalFilename();
+				String destFilePath = resourceConfigBean.getLocalPathForWindow() + fileName;
+				File destFile = new File(destFilePath);
+				file.transferTo(destFile);
+				
+				isEmpty = false;
+			}
+		} catch (IllegalStateException | IOException e) {
+			e.printStackTrace();
+			redirectAttributes.addFlashAttribute("message", "Upload file failed.");
+			return "redirect:/test/index";
+		}
+		
+		if (isEmpty) {
+			redirectAttributes.addFlashAttribute("message", "Please select file.");
+		} else {
+			redirectAttributes.addFlashAttribute("message", "Upload file success.");
+		}
+		
+		return "redirect:/test/index";
+	}
+
+	/**
+	 * 127.0.0.1/test/file ---- post
+	 */
+	@PostMapping(value = "/file", consumes = "multipart/form-data")
+	public String uploadFile(@RequestParam MultipartFile file, RedirectAttributes redirectAttributes) {
+		
+		if (file.isEmpty()) {
+			redirectAttributes.addFlashAttribute("message", "Please select file");
+			return "redirect:/test/index";
+		}
+		
+		String absolutePath = resourceConfigBean.getLocalPathForWindow() + file.getOriginalFilename();
+		String relativePath = resourceConfigBean.getResourcePath() + file.getOriginalFilename();
+		try {
+			File destFile = new File(absolutePath);
+			file.transferTo(destFile);
+			
+			// 使用工具类 Files 来上传文件
+//			byte[] bytes = file.getBytes();
+//			Path path = Paths.get(absolutePath);
+//			Files.write(path, bytes);
+		
+		} catch (IllegalStateException | IOException e) {
+			e.printStackTrace();
+			redirectAttributes.addFlashAttribute("message", "Upload fail.");
+			return "redirect:/test/index";
+		}
+		
+		redirectAttributes.addFlashAttribute("message", "Upload success.");
+		redirectAttributes.addFlashAttribute("relativePath", relativePath);
+		
+		return "redirect:/test/index";
+	}
 	
 	/**
 	 * 127.0.0.1/test/index ---- get
