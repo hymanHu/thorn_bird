d06d6408a49454eb728b1b2c2569900967029b04
HymanHu <hujiangyx@163.com>
2021-05-23 20:01:02 +0800

    delete django

--- python_django/app_spider/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppSpiderConfig(AppConfig):
-    name = 'app_spider'
--- python_django/python_django/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-import pymysql
-
-__author__ = "HymanHu";
-
-# Django 使用 pymysql 模块连接 mysql 数据库
-pymysql.install_as_MySQLdb()
