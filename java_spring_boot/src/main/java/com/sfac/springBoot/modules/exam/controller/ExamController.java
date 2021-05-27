3d6e890390a557e11eb347cb2bec1433defdcca7
hymanHu <hujiangyx@163.com>
2021-05-27 16:47:02 +0800

    "试题列表页面"

---
+++ java_spring_boot/src/main/java/com/sfac/springBoot/modules/exam/controller/ExamController.java
@@ -0,0 +1,24 @@
+package com.sfac.springBoot.modules.exam.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Description: Exam Controller
+ * @author HymanHu
+ * @date 2021-05-27 15:01:02
+ */
+@Controller
+@RequestMapping("/exam")
+public class ExamController {
+	
+	/**
+	 * 127.0.0.1/exam/questions ---- get
+	 */
+	@GetMapping("/questions")
+	public String questionsPage() {
+		return "index";
+	}
+
+}
