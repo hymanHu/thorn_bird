6b331397483ddfc7782ca3b592fbe680d82e410e
HymanHu <hujiangyx@163.com>
2021-06-21 21:55:16 +0800

    Delete Python Base

--- python_base/com/thornBird/base/CharTest.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import chardet;
-
-# ---- chardet检测编码 ----
-# 参数书bytes类型
-print(chardet.detect(b'hello world')); # ascii
-print(chardet.detect(u'你好世界'.encode())); # utf-8
-print(chardet.detect('离离原上草，一岁一枯荣'.encode('gbk'))); # GB2312
-print(chardet.detect( '最新の主要ニュース'.encode('euc-jp'))); # EUC-JP
